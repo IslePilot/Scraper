1dc056ec9294c9759036b47c2e9f850ecc7291e7
Keith Barr <keith.barr@aerosys-eng.com>
2017-10-15 14:05:54 -0600

    Initial Operation--not scraping catalyst data

--- scraper.py
+++ scraper.py
@@ -1,15 +1,13 @@
 from urllib2 import urlopen
 import re
 from yahoo_finance import Share
-
-
-
+import datetime
 from bs4 import BeautifulSoup
-from matplotlib import ticker
-from pandas.io.data import _adjust_prices
+from networkx.algorithms.flow.capacityscaling import capacity_scaling
+
 
 class StockData(object):
-    HEADER = "Ticker,Price,Drug,Indication,Phase,Date,Catalyst,Mean Recommendation,Strong Buy,Buy,Recommendation Count,Target Min,Target Mean,Target High,Growth,Market Cap,Short Ratio\n"
+    HEADER = "Ticker,Price ($),Drug,Indication,Phase,Date(YMD),Catalyst,Mean Recommendation,Strong Buy,Buy,Recommendation Count,Target Min($),Target Mean($),Target High($),Growth(%),Market Cap(M),Short Ratio(Months)\n"
     
     def __init__(self, ticker, price, drug, indication, phase, date, catalyst):
         self.ticker = ticker
@@ -23,15 +21,30 @@ class StockData(object):
         return
     
     def add_stock_data(self):
-        stock = Share(self.ticker)
+        while True:
+            try:
+                stock = Share(self.ticker)
+                break
+            except:
+                print "Error reading Yahoo: %s...retrying <<<<<<<<<<<<<<<<<<<<<<<<<<"%self.ticker
+                pass
+        
         cap = stock.get_market_cap()
-        if re.search("M", cap) != None:
-            self.market_cap = float(cap.strip("M"))*1000000
-        elif re.search("B", cap) != None:
-            self.market_cap = float(cap.strip("B"))*1000000000
+        if cap != None:
+            if re.search("M", cap) != None:
+                self.market_cap = float(cap.strip("M"))
+            elif re.search("B", cap) != None:
+                self.market_cap = float(cap.strip("B"))*1000.0
+            else:
+                self.market_cap = float(cap)/1000000.0
         else:
-            self.market_cap = float(cap)
-        self.short_ratio = float(stock.get_short_ratio())
+            self.market_cap = 0.0
+        
+        sr = stock.get_short_ratio()
+        if sr != None:
+            self.short_ratio = float(sr)
+        else:
+            self.short_ratio = 0.0
         
         print "Market Cap: %.3f"%self.market_cap
         print "Short ratio: %.2f"%self.short_ratio
@@ -57,7 +70,7 @@ class StockData(object):
         string += "%s,"%self.drug
         string += "%s,"%self.indication
         string += "%s,"%self.phase
-        string += "%s,"%self.date
+        string += "%s,"%self.date.strftime("%Y-%m-%d")
         string += "%s,"%self.catalyst
         string += "%.1f,"%self.recommendation_mean
         string += "%d,"%self.strong_buy
@@ -94,33 +107,58 @@ class FDACatalysts(object):
         stock_info = self.soup.find_all("tr")
         
         count = 0
+        ticker = ""
+        price = 0.0
+        drug = ""
+        indication = ""
+        phase = ""
+        date = None
+        catalyst = ""
         self.stock_list = []
         for stock in stock_info:
-            s1 = stock.findAll("td")
+            th = stock.find_all('th')
+            if len(th) > 0:
+                print "Found Header Record"
+                print "============================================================================"  
+                continue
             
-            if len(s1) > 0:
-                count += 1
-                ticker = s1[0].get_text()
-                price =  float(s1[1].find_all("div", class_="price")[0].get_text().split("$")[1])
-                drug = s1[2].find_all("strong", class_="drug")[0].get_text()
-                indication = s1[2].find_all("div", class_="indication")[0].get_text().replace(",", " ")
-                phase =  s1[3].get_text().encode("ascii", "ignore").strip()
-                date_string =  s1[4].find_all("time", class_="catalyst-date")[0].get_text()
-                catalyst =  s1[4].find_all("div", class_="catalyst-note")[0].get_text().replace(",", " ")
-                
-                # build our StockData object
-                self.stock_list.append(StockData(ticker, price, drug, 
-                                                 indication, phase, date_string, catalyst))
-                print "Count = %d"%count
-                print "Ticker = %s"%ticker
-                print "Price = %.2f"%price
-                print "Drug = %s"%drug
-                print "Indication = %s"%indication
-                print "Phase = %s"%phase
-                print "Date = %s"%date_string
-                print "Catalyst = %s"%catalyst
-            print "============================================================================"
+            count += 1
+            
+            for a in stock.find_all('a', class_="ticker"):
+                ticker =  a.get_text().encode("ascii", "ingore")
+            
+            for div in stock.find_all('div', class_="price"):
+                price = float(div.get_text().split("$")[1])
+            
+            for strong in stock.find_all('strong', class_='drug'):
+                drug = strong.get_text().encode("ascii", "ignore")
+            
+            for div in stock.find_all('div', class_='indication'):
+                indication = div.get_text().replace(",", " ").replace("\n"," ").encode("ascii", "ignore")
+            
+            for td in stock.find_all('td', class_="stage"):
+                phase = td.get_text().encode("ascii", "ignore").strip()
+            
+            for time in stock.find_all('time', class_="catalyst-date"):
+                d = time.get_text()
+                date = datetime.datetime.strptime(d, "%m/%d/%Y").date()
+            
+            for div in stock.find_all('div', class_="catalyst-note"):
+                catalyst = div.get_text().replace(",", " ").replace("\n"," ").encode("ascii", "ignore")
+            
+            print "Ticker = %s"%ticker
+            print "Price = %.2f"%price
+            print "Drug = %s"%drug
+            print "Indication = %s"%indication
+            print "Phase = %s"%phase
+            print "Date = %s"%date.strftime("%m/%d/%Y")
+            print "Catalyst = %s"%catalyst
+            print "Count = %d"%count
+            print "============================================================================"   
 
+            # build our StockData object
+            self.stock_list.append(StockData(ticker, price, drug, indication, phase, date, catalyst))
+        
         return
 
 class AnalystData(object):
@@ -138,41 +176,59 @@ class AnalystData(object):
         return
     
     def parse_html(self):
+        self.recommendation_mean = 0.0
+        self.strong_buy = 0
+        self.buy = 0
+        self.rec_count = 0
+        self.target_low = 0.0
+        self.target_mean = 0.0
+        self.target_high = 0.0
+        self.current_price = 0.0
+        self.target_growth = 0.0
+        
         # find the average recommendation
         # {"recommendationMean":{"raw":1.7,"fmt":"1.70"}}
-        self.recommendation_mean = float(self.get_string("recommendationMean").split('"')[5])
+        l = self.get_string("recommendationMean").split('"')
+        if len(l) > 5:
+            self.recommendation_mean = float(l[5])
         #print "Recommendation Mean: %.2f"%self.recommendation_mean
         
         # {"recommendationTrend":{"trend":[{"period":"0m","strongBuy":3,"buy":6,"hold":0,"sell":0,"strongSell":0},{"period":"-1m","strongBuy":3,"buy":5,"hold":0,"sell":0,"strongSell":0},{"period":"-2m","strongBuy":3,"buy":5,"hold":0,"sell":0,"strongSell":0},{"period":"-3m","strongBuy":3,"buy":6,"hold":0,"sell":0,"strongSell":0}],"maxAge":86400}
         data = self.get_string("recommendationTrend")
         parsed_data = re.split(":|,", data)
-        
-        self.strong_buy = int(parsed_data[4])
-        self.buy = int(parsed_data[6])
-        
+        if len(parsed_data) > 6:
+            self.strong_buy = int(parsed_data[4])
+            self.buy = int(parsed_data[6])
+            self.rec_count = self.strong_buy + self.buy
         #print "Strong Buy: %d"%self.strong_buy
         #print "Buy: %d"%self.buy
-        self.rec_count = self.strong_buy + self.buy
         #print "Reccomendation Count: %d"%self.rec_count
         
-        # "currentPrice":{"raw":25.76,"fmt":"25.76"}
-        self.current_price = float(self.get_string("currentPrice").split('"')[5])
-        #print "Current Price: %.2f"%self.current_price
-        
         # "targetLowPrice":{"raw":37,"fmt":"37.00"}
-        self.target_low = float(self.get_string("targetLowPrice").split('"')[5])
+        l = self.get_string("targetlowPrice").split('"')
+        if len(l) > 5:
+            self.target_low = float(l[5])
         #print "Target Low Price: %.2f"%self.target_low
         
         # "targetMeanPrice":{"raw":39.22,"fmt":"39.22"}
-        self.target_mean = float(self.get_string("targetMeanPrice").split('"')[5])
+        l = self.get_string("targetMeanPrice").split('"')
+        if len(l) > 5:
+            self.target_mean = float(l[5])
         #print "Target Mean Price: %.2f"%self.target_mean
         
         # "targetHighPrice":{"raw":44,"fmt":"44.00"}
-        self.target_high = float(self.get_string("targetHighPrice").split('"')[5])
+        l = self.get_string("targetHighPrice").split('"')
+        if len(l) > 5:
+            self.target_high = float(l[5])
         #print "Target High Price: %.2f"%self.target_high
         
-        self.target_growth = 100.0*(self.target_mean-self.current_price)/self.current_price
+        # "currentPrice":{"raw":25.76,"fmt":"25.76"}
+        l = self.get_string("currentPrice").split('"')
+        if len(l) > 5:
+            self.current_price = float(l[5])
+            self.target_growth = 100.0*(self.target_mean-self.current_price)/self.current_price
         #print "Target Growth %.1f%%"%self.target_growth
+        #print "Current Price: %.2f"%self.current_price
         
         print "%s: Growth:%.1f MeanRecommendation:%.1f AnalystCount:%d"%(self.ticker,
                                                                          self.target_growth,
@@ -182,6 +238,9 @@ class AnalystData(object):
     
     def get_string(self, key):
         # the format is always "key":{string}
+        # examples:
+        # "targetLowPrice":{"raw":37,"fmt":"37.00"}
+        # "targetLowPrice":{}
         # find the location of the key
         key_start = self.html.find(key)
         open_brace = self.html.find("{", key_start)
@@ -204,13 +263,14 @@ if __name__ == '__main__':
     count = 0
     for stock in stock_list:
         count += 1
-        stock.add_stock_data()
-        adata = AnalystData(stock.ticker)
-        stock.add_analyst_data(*adata.get_data())
-        print stock.get_csv()
-        csv_file.write(stock.get_csv())
-        if count >= 20:
-            break
+        if count >= 0:
+            print "%d: %s"%(count, stock.ticker)
+            stock.add_stock_data()
+            adata = AnalystData(stock.ticker)
+            stock.add_analyst_data(*adata.get_data())
+            print stock.get_csv()
+            csv_file.write(stock.get_csv())
+
     
 
 
